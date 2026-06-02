12674f36dbbb2880069f4bda20d722c4d71425a3
Jaccstudios <juliocc817@gmail.com>
2026-06-02 13:10:51 -0500

    Initial Commit

---
+++ QuePersonajeEres/QuePersonajeEresApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuePersonajeEresApp.swift
+//  QuePersonajeEres
+//
+//  Created by user948554 on 6/2/26.
+//
+
+import SwiftUI
+
+@main
+struct QuePersonajeEresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
